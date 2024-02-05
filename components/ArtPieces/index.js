df5f722a5c5059114b90d42d2efd0dd645d06493
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-05 12:53:45 +0100

    adding list of art pieces

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,8 +1,18 @@
+import { ArtPiecePreview, CardWithStats } from "../ArtPiecePreview";
+
 export default function ArtPieces({ pieces }) {
   return (
     <ul>
       {pieces.map((piece) => {
-        return <li key={piece.slug}>{piece.artist}</li>;
+        return (
+          <li key={piece.slug}>
+            <ArtPiecePreview
+              image={piece.imageSource}
+              artist={piece.artist}
+              title={piece.name}
+            />
+          </li>
+        );
       })}
     </ul>
   );
