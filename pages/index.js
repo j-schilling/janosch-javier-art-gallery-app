f5a9eb4025c1ef032c23029ea21ee169d83c1fce
jamoragar <javier.moragarojas@gmail.com>
2024-02-05 12:55:57 +0100

    removing console.log

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function HomePage() {
   if (isLoading) return <h1>Loading...</h1>;
   if (error) return <h1>Error...</h1>;
 
-  console.log(data);
   return (
     <div>
       <ArtPieces pieces={data} />
