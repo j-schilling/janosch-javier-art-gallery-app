01631158c2f9abbd351ad0d9e9055ecfe2e7aec9
jamoragar <javier.moragarojas@gmail.com>
2024-02-05 13:08:05 +0100

    style[card]: Adding styles to the card component

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -1,11 +1,12 @@
 import { ArtPiecePreview, CardWithStats } from "../ArtPiecePreview";
+import classes from "./ArtPieces.module.css";
 
 export default function ArtPieces({ pieces }) {
   return (
-    <ul>
+    <ul className={classes.ul}>
       {pieces.map((piece) => {
         return (
-          <li key={piece.slug}>
+          <li key={piece.slug} className={classes.li}>
             <ArtPiecePreview
               image={piece.imageSource}
               artist={piece.artist}
