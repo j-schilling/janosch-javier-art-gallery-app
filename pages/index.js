ce05dfecba2973f5a062a28a0e3e804cf458dc80
jamoragar <javier.moragarojas@gmail.com>
2024-02-05 10:40:57 +0100

    feat[fetch]: Fetching data from the API with SWR

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
+import useSWR from "swr";
+
+const API_URL = "https://example-apis.vercel.app/api/art";
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(API_URL);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error...</h1>;
+
+  console.log(data);
   return (
     <div>
       <h1>Hello from Next.js</h1>
