3b60d123dabaad2e622771320f0d2817e1de7e95
jamoragar <javier.moragarojas@gmail.com>
2024-02-06 16:03:12 +0100

    fix[favPage]: Removing extra data with spread operator

--- pages/favorites.js
+++ pages/favorites.js
@@ -2,15 +2,15 @@ import { useArtPieces } from "@/context/ArtPiecesContext";
 import { ArtPiecePreview } from "@/components/ArtPiecePreview";
 import classes from "../components/ArtPieces/ArtPieces.module.css";
 
-export default function Favorites({ globalData }) {
+export default function Favorites() {
   const { artPiecesInfo } = useArtPieces();
 
   const favoritePieces = Object.entries(artPiecesInfo)
-    .filter(([slug, info]) => info.isFavorite)
-    .map(([slug, info]) => ({
-      slug,
-      ...info,
+    .filter(([slug, piece]) => piece.isFavorite)
+    .map(([slug, piece]) => ({
+      ...piece,
     }));
+  console.log(favoritePieces);
 
   return (
     <div>
@@ -18,12 +18,12 @@ export default function Favorites({ globalData }) {
       <ul className={classes.ul}>
         {favoritePieces.length > 0 ? (
           favoritePieces.map((piece) => (
-            <li className={classes.li} key={piece.slug}>
+            <li key={piece.slug} className={classes.li}>
               <ArtPiecePreview {...piece} />
             </li>
           ))
         ) : (
-          <p>No hay piezas marcadas como favoritas.</p>
+          <p>There is no favorites pieces of art to show.</p>
         )}
       </ul>
     </div>
