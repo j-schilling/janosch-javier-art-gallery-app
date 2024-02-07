4397b657d5b09cbc8adf43083c88cc01ef0d645f
jamoragar <javier.moragarojas@gmail.com>
2024-02-07 14:02:27 +0100

    test[artPieces]: configuration with babel and mocks for testing works as expected

---
+++ components/ArtPieces/ArtPieces.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import ArtPieces from ".";
+
+jest.mock("use-local-storage-state", () => {
+  return jest.fn(() => [jest.fn(), jest.fn()]);
+});
+
+jest.mock("../ArtPiecePreview", () => ({
+  ArtPiecePreview: () => <div>Mock ArtPiecePreview</div>,
+}));
+
+describe("ArtPieces", () => {
+  test("renders correctly based on pieces prop", () => {
+    const pieces = [
+      {
+        slug: "1",
+        imageSource: "image1.jpg",
+        name: "Art Piece 1",
+        artist: "Artist 1",
+      },
+      {
+        slug: "2",
+        imageSource: "image2.jpg",
+        name: "Art Piece 2",
+        artist: "Artist 2",
+      },
+      {
+        slug: "3",
+        imageSource: "image3.jpg",
+        name: "Art Piece 3",
+        artist: "Artist 3",
+      },
+    ];
+
+    render(<ArtPieces pieces={pieces} />);
+
+    // Since we mocked ArtPiecePreview, we can't directly check for its presence.
+    // Instead, we can check for the mock's output or check that the list items are rendered.
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems).toHaveLength(pieces.length);
+  });
+});
